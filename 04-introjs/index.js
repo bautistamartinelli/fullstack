6ba123b5fcb78b4a7cae2d49e8e50e2ef299726c
Juan Bautista Martinelli <bauti1212.martinelli@gmail.com>
2024-06-18 21:31:28 -0300

    intro a JS

---
+++ 04-introjs/index.js
@@ -0,0 +1,16 @@
+//estos son los comentarios con dos // en js
+
+
+/*
+estos son los comentarios
+con varias lineas
+*/
+
+
+//Variables: espacios de memoria para almacenar datos
+//1. se inicia con var
+// palabras reservadas: son utilizadas solo para las definiciones del lenguaje de programacion
+var nombre;
+var email
+var edad
+var pais;
